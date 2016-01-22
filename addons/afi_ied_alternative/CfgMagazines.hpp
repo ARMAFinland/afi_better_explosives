b4f43a7b68b8af7ae0c0eb533350e35554b6a068
Antti Sonkeri <antti.sonkeri@gmail.com>
2016-01-22 20:32:17 +0200

    Clean configs. Add better IED descriptions.

--- addons/afi_ied_alternative/CfgMagazines.hpp
+++ addons/afi_ied_alternative/CfgMagazines.hpp
@@ -4,8 +4,8 @@ class CfgMagazines
 	class PressurePlate;
 	class IEDUrbanBig_Remote_Mag: DemoCharge_Remote_Mag
 	{
-		displayName = "Big IED (Urban)";
-		descriptionShort="122mm artillery shell.";
+		displayName = "Big urban IED";
+		descriptionShort="Explosive device improvised from a 122mm artillery shell.";
 		ACE_SetupObject = "ACE_Explosives_Place_IEDUrbanBig";
 		class ACE_Triggers
 		{
@@ -21,19 +21,11 @@ class CfgMagazines
 	};
 	class IEDUrbanBig_Pressure_Mag: IEDUrbanBig_Remote_Mag
 	{
-		displayName = "Big IED pressure plate (Urban)";
-		descriptionShort="122mm artillery shell";
-		ACE_SetupObject = "ACE_Explosives_Place_IEDUrbanBig";
+		displayName = "Big urban IED (pressure plate)";
+		descriptionShort="Explosive device improvised from a 122mm artillery shell.";
 		class ACE_Triggers
 		{
 			SupportedTriggers[] = {"Command","DeadmanSwitch","Cellphone","PressurePlate"};
-			class Command
-			{
-				FuseTime = 0.5;
-				ammo = "IEDUrbanBig_Command_Ammo";
-			};
-			class DeadmanSwitch: Command{};
-			class Cellphone: Command{};
 			class PressurePlate
 			{
 				displayName = "$STR_ace_explosives_PressurePlate";
@@ -45,8 +37,8 @@ class CfgMagazines
 	};
 	class IEDLandBig_Remote_Mag: IEDUrbanBig_Remote_Mag
 	{
-		displayName = "Big IED (Dug-in)";
-		descriptionShort="122mm artillery shell";
+		displayName = "Big dug-in IED";
+		descriptionShort="Explosive device improvised from a 122mm artillery shell.";
 		ACE_SetupObject = "ACE_Explosives_Place_IEDLandBig";
 		class ACE_Triggers: ACE_Triggers
 		{
@@ -54,30 +46,30 @@ class CfgMagazines
 			{
 				ammo = "IEDLandBig_Command_Ammo";
 			};
+			class DeadmanSwitch: Command {};
+			class Cellphone: Command {};
 		};
 	};
 	class IEDLandBig_Pressure_Mag: IEDLandBig_Remote_Mag
 	{
-		displayName = "Big IED pressure plate (Dug-in)";
-		descriptionShort="122mm artillery shell";
+		displayName = "Big dug-in IED (pressure plate)";
+		descriptionShort="Explosive device improvised from a 122mm artillery shell.";
 		ACE_SetupObject = "ACE_Explosives_Place_IEDLandBig";
 		class ACE_Triggers: ACE_Triggers
 		{
 			SupportedTriggers[] = {"Command","DeadmanSwitch","Cellphone","PressurePlate"};
-			class Command: Command
-			{
-				ammo = "IEDLandBig_Command_Ammo";
-			};
 			class PressurePlate: PressurePlate
 			{
 				ammo = "IEDLandBig_Range_Ammo";
 			};
+			class DeadmanSwitch: Command {};
+			class Cellphone: Command {};
 		};
 	};
 	class IEDUrbanSmall_Remote_Mag: DemoCharge_Remote_Mag
 	{
-		displayName = "Small IED (Urban)";
-		descriptionShort="Hand granade";
+		displayName = "Small urban IED";
+		descriptionShort="Explosive device improvised from a hand grenade.";
 		ACE_SetupObject = "ACE_Explosives_Place_IEDUrbanSmall";
 		class ACE_Triggers
 		{
@@ -93,19 +85,11 @@ class CfgMagazines
 	};
 	class IEDUrbanSmall_Pressure_Mag: IEDUrbanSmall_Remote_Mag
 	{
-		displayName = "Small IED pressure plate (Urban)";
-		descriptionShort="Hand granade";
-		ACE_SetupObject = "ACE_Explosives_Place_IEDUrbanSmall";
+		displayName = "Small urban IED (pressure plate)";
+		descriptionShort="Explosive device improvised from a hand grenade.";
 		class ACE_Triggers
 		{
 			SupportedTriggers[] = {"Command","DeadmanSwitch","Cellphone","PressurePlate"};
-			class Command
-			{
-				FuseTime = 0.5;
-				ammo = "IEDUrbanSmall_Command_Ammo";
-			};
-			class DeadmanSwitch: Command{};
-			class Cellphone: Command{};
 			class PressurePlate
 			{
 				displayName = "$STR_ace_explosives_PressurePlate";
@@ -117,8 +101,8 @@ class CfgMagazines
 	};
 	class IEDLandMedium_Remote_Mag: IEDUrbanSmall_Remote_Mag
 	{
-		displayName = "Medium IED (Dug-in)";
-		descriptionShort="81mm mortar bomb";
+		displayName = "Medium dug-in IED";
+		descriptionShort="Explosive device improvised from a 81mm mortar shell.";
 		ACE_SetupObject = "ACE_Explosives_Place_IEDLandSmall";
 		class ACE_Triggers: ACE_Triggers
 		{
@@ -126,20 +110,19 @@ class CfgMagazines
 			{
 				ammo = "IEDLandSmall_Command_Ammo";
 			};
+			class DeadmanSwitch: Command {};
+			class Cellphone: Command {};
 		};
 	};
 	class IEDLandMedium_Pressure_Mag: IEDLandMedium_Remote_Mag
 	{
-		displayName = "Medium IED pressure plate (Dug-in)";
-		descriptionShort="81mm mortar bomb";
-		ACE_SetupObject = "ACE_Explosives_Place_IEDLandSmall";
+		displayName = "Medium dug-in IED (pressure plate)";
+		descriptionShort="Explosive device improvised from a 81mm mortar shell.";
 		class ACE_Triggers: ACE_Triggers
 		{
 			SupportedTriggers[] = {"Command","DeadmanSwitch","Cellphone","PressurePlate"};
-			class Command: Command
-			{
-				ammo = "IEDLandSmall_Command_Ammo";
-			};
+			class DeadmanSwitch: Command {};
+			class Cellphone: Command {};
 			class PressurePlate: PressurePlate
 			{
 				ammo = "IEDLandSmall_Range_Ammo";
