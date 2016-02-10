62f494c5f4475bbed1a4547a0032f3d076d1f964
Johnson <johanj3@DTF507542.espoo.ad.city>
2016-02-10 17:20:20 +0200

    Fixed missing semicolon

--- addons/afi_apers_alternative/CfgMagazines.hpp
+++ addons/afi_apers_alternative/CfgMagazines.hpp
@@ -4,25 +4,25 @@ class CfgMagazines {
 		displayName = "Tripflare (white)";
 		ammo = "Tripflare_Ammo_White";
 		descriptionShort="Tripflare that launches a white flare high above its position.";
-		picture = "\afi_apers_alternative\Data\UI\gear_mine_tripflare_white"
+		picture = "\afi_apers_alternative\Data\UI\gear_mine_tripflare_white";
 	};
 	class Tripflare_Mag_Red: APERSTripMine_Wire_Mag {
 		displayName = "Tripflare (red)";
 		ammo = "Tripflare_Ammo_Red";
 		descriptionShort="Tripflare that launches a red flare high above its position.";
-		picture = "\afi_apers_alternative\Data\UI\gear_mine_tripflare_red"
+		picture = "\afi_apers_alternative\Data\UI\gear_mine_tripflare_red";
 	};
 	class Tripflare_Mag_Yellow: APERSTripMine_Wire_Mag {
 		displayName = "Tripflare (yellow)";
 		ammo = "Tripflare_Ammo_Yellow";
 		descriptionShort="Tripflare that launches a yellow flare high above its position.";
-		picture = "\afi_apers_alternative\Data\UI\gear_mine_tripflare_yellow"
+		picture = "\afi_apers_alternative\Data\UI\gear_mine_tripflare_yellow";
 	};
 	class Tripflare_Mag_Green: APERSTripMine_Wire_Mag {
 		displayName = "Tripflare (green)";
 		ammo = "Tripflare_Ammo_Green";
 		descriptionShort="Tripflare that launches a green flare high above its position.";
-		picture = "\afi_apers_alternative\Data\UI\gear_mine_tripflare_green"
+		picture = "\afi_apers_alternative\Data\UI\gear_mine_tripflare_green";
 	};
 
 	//Ground Flare variants
@@ -30,24 +30,24 @@ class CfgMagazines {
 		displayName = "Ground tripflare (white)";
 		ammo = "Tripflare_Ammo_Ground_White";
 		descriptionShort="Tripflare that activates a white flare on the ground.";
-		picture = "\afi_apers_alternative\Data\UI\gear_mine_tripflare_white"
+		picture = "\afi_apers_alternative\Data\UI\gear_mine_tripflare_white";
 	};
 	class Tripflare_Mag_Ground_Red: APERSTripMine_Wire_Mag {
 		displayName = "Ground tripflare (red)";
 		ammo = "Tripflare_Ammo_Ground_Red";
 		descriptionShort="Tripflare that activates a red flare on the ground.";
-		picture = "\afi_apers_alternative\Data\UI\gear_mine_tripflare_red"
+		picture = "\afi_apers_alternative\Data\UI\gear_mine_tripflare_red";
 	};
 	class Tripflare_Mag_Ground_Yellow: APERSTripMine_Wire_Mag {
 		displayName = "Ground tripflare (yellow)";
 		ammo = "Tripflare_Ammo_Ground_Yellow";
 		descriptionShort="Tripflare that activates a yellow flare on the ground.";
-		picture = "\afi_apers_alternative\Data\UI\gear_mine_tripflare_yellow"
+		picture = "\afi_apers_alternative\Data\UI\gear_mine_tripflare_yellow";
 	};
 	class Tripflare_Mag_Ground_Green: APERSTripMine_Wire_Mag {
 		displayName = "Ground tripflare (green)";
 		ammo = "Tripflare_Ammo_Ground_Green";
 		descriptionShort="Tripflare that activates a green flare on the ground.";
-		picture = "\afi_apers_alternative\Data\UI\gear_mine_tripflare_green"
+		picture = "\afi_apers_alternative\Data\UI\gear_mine_tripflare_green";
 	};
 };
